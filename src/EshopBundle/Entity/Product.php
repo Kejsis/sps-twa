9df493b85dc2b913185206118c6b8984d11129e5
Tomas Kment <Kment.Tom@seznam.cz>
2018-01-22 14:50:36 +0100

    Vytoření Entitní třídy

---
+++ src/EshopBundle/Entity/Product.php
@@ -0,0 +1,54 @@
+<?php
+namespace EshopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Product
+ *
+ * @author kmento
+ * @ORM\Entity()
+ */
+class Product {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(type="string",length=128, nullable=true)
+     */
+    private $name;
+    
+    
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+
+}
